22ff92fe2aad15e254b011367139980ac6026054
freeany <812974019@qq.com>
2020-12-28 22:18:07 +0800

    feat: sass-loader传递属性使用全局css变量

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,7 @@
     <h1>App.vue</h1>
     <router-view />
 
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
+    <div class="text">hello world</div>
   </div>
 </template>
 <script>
@@ -71,4 +64,9 @@ export default class App extends Vue {
   }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// @import '~@/styles/index.scss';
+.text {
+  color: $warning-color;
+}
+</style>
